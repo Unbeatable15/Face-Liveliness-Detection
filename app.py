fc53131923e33fa12a6cb762615b568cf4e68519
Ajinkya Kshatriya <ajinkyak1510@gmail.com>
2023-12-13 23:55:48 +0530

    sixth commit

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from flask import Flask, render_template, Response
 import cv2
-from keras_preprocessing.image import img_to_array
 import numpy as np
 from tensorflow.keras.models import model_from_json
 
 app = Flask(__name__)
 
 # Load Face Detection Model
-face_cascade = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Load Anti-Spoofing Model graph
-json_file = open('antispoofing_models/antispoofing_model_mobilenet.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
+with open('antispoofing_models/antispoofing_model_mobilenet.json', 'r') as json_file:
+    loaded_model_json = json_file.read()
 
-# load antispoofing model weights
+model = model_from_json(loaded_model_json)
 model.load_weights('antispoofing_models/antispoofing_model_40-0.995714.h5')
 print("Model loaded from disk")
 
@@ -34,14 +31,11 @@ def process_frame(frame):
         # model to determine if the face is "real" or "fake"
         preds = model.predict(resized_face)[0]
 
-        if preds > 0.5:
-            label = 'spoof'
-            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        else:
-            label = 'real'
-            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        label = 'spoof' if preds > 0.5 else 'real'
+        color = (0, 0, 255) if label == 'spoof' else (0, 255, 0)
+
+        cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
 
     return frame
 
@@ -52,12 +46,13 @@ def generate_frames():
         success, frame = video.read()
         if not success:
             break
-        else:
-            processed_frame = process_frame(frame)
-            ret, buffer = cv2.imencode('.jpg', processed_frame)
-            frame_bytes = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        processed_frame = process_frame(frame)
+        ret, buffer = cv2.imencode('.jpg', processed_frame)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+    video.release()
 
 
 @app.route('/')
