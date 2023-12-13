8a8a4d2e4724fefb07a975f4941c368f4573abce
Ajinkya Kshatriya <ajinkyak1510@gmail.com>
2023-12-14 00:13:46 +0530

    seventh commit

--- app.py
+++ app.py
@@ -5,19 +5,20 @@ from tensorflow.keras.models import model_from_json
 
 app = Flask(__name__)
 
-# Load Face Detection Model
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-# Load Anti-Spoofing Model graph
-with open('antispoofing_models/antispoofing_model_mobilenet.json', 'r') as json_file:
-    loaded_model_json = json_file.read()
-
+json_file = open('antispoofing_models/antispoofing_model_mobilenet.json', 'r')
+loaded_model_json = json_file.read()
 model = model_from_json(loaded_model_json)
 model.load_weights('antispoofing_models/antispoofing_model_40-0.995714.h5')
 print("Model loaded from disk")
 
+video = None  # Initialize video capture instance
 
 def process_frame(frame):
+    global video  # Access the global video object
+    if video is None:  # Check if video capture instance is not initialized
+        video = cv2.VideoCapture(0)
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
@@ -27,8 +28,6 @@ def process_frame(frame):
         resized_face = resized_face.astype("float") / 255.0
         resized_face = np.expand_dims(resized_face, axis=0)
 
-        # pass the face ROI through the trained liveness detector
-        # model to determine if the face is "real" or "fake"
         preds = model.predict(resized_face)[0]
 
         label = 'spoof' if preds > 0.5 else 'real'
@@ -39,9 +38,7 @@ def process_frame(frame):
 
     return frame
 
-
 def generate_frames():
-    video = cv2.VideoCapture(0)
     while True:
         success, frame = video.read()
         if not success:
@@ -52,18 +49,20 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
-    video.release()
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    global video
+    if video is not None:
+        video.release()
+    return 'Server shutting down...'
 
 if __name__ == "__main__":
     app.run(debug=True)
